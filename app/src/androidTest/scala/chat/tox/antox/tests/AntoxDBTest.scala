34e115939ed6f70a2d20d56b167f6bfaa4c882e6
wiiam <wiiam@tuta.io>
2015-10-14 21:18:47 +1300

    Add unreadmessages test

--- app/src/androidTest/scala/chat/tox/antox/tests/AntoxDBTest.scala
+++ app/src/androidTest/scala/chat/tox/antox/tests/AntoxDBTest.scala
@@ -3,10 +3,13 @@ package chat.tox.antox.tests
 import android.preference.PreferenceManager
 import android.support.test.runner.AndroidJUnit4
 import android.test.{AndroidTestCase, RenamingDelegatingContext}
-import chat.tox.antox.data.AntoxDB
-import chat.tox.antox.utils.SelfKey
-import chat.tox.antox.wrapper.{FriendKey, MessageType, ToxKey}
-import im.tox.tox4j.core.enums.ToxMessageType
+import chat.tox.antox.data.{State, AntoxDB}
+import chat.tox.antox.tox.{ToxDataFile, ToxSingleton}
+import chat.tox.antox.utils.{Options, SelfKey}
+import chat.tox.antox.wrapper._
+import im.tox.tox4j.core.enums.{ToxSavedataType, ToxUserStatus, ToxMessageType}
+import im.tox.tox4j.core.options.{SaveDataOptions, ToxOptions}
+import im.tox.tox4j.exceptions.ToxException
 import org.junit.Assert._
 import org.junit.runner.RunWith
 import org.junit.{After, Before, Test}
@@ -18,12 +21,11 @@ class AntoxDBTest extends AndroidTestCase {
 
   private var db: AntoxDB = _
 
+  val context = new RenamingDelegatingContext(getContext, "test_")
+
   @Before
   override def setUp(): Unit = {
     super.setUp()
-
-    val context = new RenamingDelegatingContext(getContext, "test_")
-
     val preferences = PreferenceManager.getDefaultSharedPreferences(context)
     db = new AntoxDB(context, "jim", selfKey)
   }
@@ -33,13 +35,13 @@ class AntoxDBTest extends AndroidTestCase {
     super.tearDown()
   }
 
-  val selfKey = new SelfKey("11BB3CCDD46346EAA76FF935F1CB31CDC11C56803F1077745124A1C7C63F7E6C8B286B415682")
+  val selfKey = new SelfKey("A1C7C6D463F7E6211BB3DC886B41568346EAA76FFDC11C56803F107774CC5124")
 
-  val key = new FriendKey("828435142ACE09E8677427E6180BFB27E38FB589A3B84C24976AE49F80A69C68")
+  val key = new FriendKey("DC11C56803F107774CC5124A1C7C6D463F7E6211BB3DC886B41568346EAA76FF")
   val name = "Steve Appleseed"
   val alias = "Steve"
   val statusMessage = "This is my status"
-
+  
   @Test
   def testAddFriend(): Unit = {
     db.addFriend(key, name, alias, statusMessage)
@@ -79,5 +81,21 @@ class AntoxDBTest extends AndroidTestCase {
 
     assertEquals(number, numChanges)
   }
+
+  @Test
+  def testUnreadMessages(): Unit ={
+    db.markIncomingMessagesRead(key)
+
+    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+
+    assertEquals(5, db.getUnreadCounts(key))
+  }
+
+
+
 
 }
