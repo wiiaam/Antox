9c320f5880b77e6d6869a2a44fd3731743ac78f4
wiiam <wiiam@tuta.io>
2015-10-15 18:43:50 +1300

    Change key to friendKey

--- app/src/androidTest/scala/chat/tox/antox/tests/AntoxDBTest.scala
+++ app/src/androidTest/scala/chat/tox/antox/tests/AntoxDBTest.scala
@@ -3,13 +3,10 @@ package chat.tox.antox.tests
 import android.preference.PreferenceManager
 import android.support.test.runner.AndroidJUnit4
 import android.test.{AndroidTestCase, RenamingDelegatingContext}
-import chat.tox.antox.data.{State, AntoxDB}
-import chat.tox.antox.tox.{ToxDataFile, ToxSingleton}
-import chat.tox.antox.utils.{Options, SelfKey}
+import chat.tox.antox.data.AntoxDB
+import chat.tox.antox.utils.SelfKey
 import chat.tox.antox.wrapper._
-import im.tox.tox4j.core.enums.{ToxSavedataType, ToxUserStatus, ToxMessageType}
-import im.tox.tox4j.core.options.{SaveDataOptions, ToxOptions}
-import im.tox.tox4j.exceptions.ToxException
+import im.tox.tox4j.core.enums.ToxMessageType
 import org.junit.Assert._
 import org.junit.runner.RunWith
 import org.junit.{After, Before, Test}
@@ -21,11 +18,11 @@ class AntoxDBTest extends AndroidTestCase {
 
   private var db: AntoxDB = _
 
-  val context = new RenamingDelegatingContext(getContext, "test_")
 
   @Before
   override def setUp(): Unit = {
     super.setUp()
+    val context = new RenamingDelegatingContext(getContext, "test_")
     val preferences = PreferenceManager.getDefaultSharedPreferences(context)
     db = new AntoxDB(context, "jim", selfKey)
   }
@@ -37,14 +34,14 @@ class AntoxDBTest extends AndroidTestCase {
 
   val selfKey = new SelfKey("A1C7C6D463F7E6211BB3DC886B41568346EAA76FFDC11C56803F107774CC5124")
 
-  val key = new FriendKey("DC11C56803F107774CC5124A1C7C6D463F7E6211BB3DC886B41568346EAA76FF")
+  val friendKey = new FriendKey("DC11C56803F107774CC5124A1C7C6D463F7E6211BB3DC886B41568346EAA76FF")
   val name = "Steve Appleseed"
   val alias = "Steve"
   val statusMessage = "This is my status"
   
   @Test
   def testAddFriend(): Unit = {
-    db.addFriend(key, name, alias, statusMessage)
+    db.addFriend(friendKey, name, alias, statusMessage)
 
     db.friendInfoList.subscribe(friendList => {
       assert(friendList.size == 1)
@@ -52,30 +49,30 @@ class AntoxDBTest extends AndroidTestCase {
       assert(friendList.exists(_.alias equals alias))
       assert(friendList.exists(_.getAliasOrName equals alias))
       assert(friendList.exists(_.statusMessage equals statusMessage))
-      assert(friendList.exists(_.key equals key))
+      assert(friendList.exists(_.key equals friendKey))
     })
   }
 
   @Test
   def testLastMessages(): Unit = {
-    db.addFriend(key, name, alias, statusMessage)
+    db.addFriend(friendKey, name, alias, statusMessage)
 
     val numChanges = 5
     var number = 0
-    db.messageListObservable(Some(key))
+    db.messageListObservable(Some(friendKey))
       .subscribe(messages => {
       assertEquals(messages, ArrayBuffer.empty)
       number += 1
     })
 
     val numMessages = 1
-    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, friendKey, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
 
     db.lastMessages
       .subscribe(messages => {
       assertEquals(messages.size, numMessages)
       assert(messages.exists(_.message == "test"))
-      
+
       number += 1
     })
 
@@ -84,15 +81,15 @@ class AntoxDBTest extends AndroidTestCase {
 
   @Test
   def testUnreadMessages(): Unit ={
-    db.markIncomingMessagesRead(key)
+    db.markIncomingMessagesRead(friendKey)
 
-    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
-    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
-    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
-    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
-    db.addMessage(-1, key, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, friendKey, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, friendKey, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, friendKey, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, friendKey, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
+    db.addMessage(-1, friendKey, selfKey, "asdf", "test", hasBeenReceived = false, hasBeenRead = false, successfullySent = true, ToxMessageType.NORMAL)
 
-    assertEquals(5, db.getUnreadCounts(key))
+    assertEquals(5, db.getUnreadCounts(friendKey))
   }
 
 
